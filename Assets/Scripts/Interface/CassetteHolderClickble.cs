ae5878308059d5d013865fadb896d43f50f9cc41
= <=>
2026-07-24 11:03:28 -0400

    cassette click added

---
+++ Assets/Scripts/Interface/CassetteHolderClickble.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using UnityEngine;
+
+public class CassetteHolderClickble : MonoBehaviour, IClickable
+{
+    [SerializeField] private float animationTime = 0.3f;
+    [SerializeField] private AnimationCurve rotationCurve = AnimationCurve.EaseInOut(0f, 0f, 1f, 1f);
+    [SerializeField] private Vector3 openLocalEuler;
+
+    private Vector3 _closedLocalEuler;
+    private bool _isOpen;
+    private bool _isAnimating;
+    private Coroutine _rotateRoutine;
+
+    private void Start()
+    {
+        _closedLocalEuler = transform.localEulerAngles;
+    }
+
+    public void OnClick()
+    {
+        if (_isAnimating) return;
+
+        _isOpen = !_isOpen;
+        Vector3 target = _isOpen ? openLocalEuler : _closedLocalEuler;
+
+        if (_rotateRoutine != null) StopCoroutine(_rotateRoutine);
+        _rotateRoutine = StartCoroutine(RotateTo(target));
+    }
+
+    private IEnumerator RotateTo(Vector3 target)
+    {
+        _isAnimating = true;
+        Vector3 start = transform.localEulerAngles;
+        float t = 0f;
+
+        while (t < animationTime)
+        {
+            t += Time.deltaTime;
+            float pct = rotationCurve.Evaluate(t / animationTime);
+            transform.localEulerAngles = Vector3.Lerp(start, target, pct);
+            yield return null;
+        }
+
+        transform.localEulerAngles = target;
+        _isAnimating = false;
+    }
+
+    public bool IsOpen => _isOpen;
+}
