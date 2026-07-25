34022d88fba44cc995fd6da1e5482c0c0929efa2
Henry Hammer <hammerthenry@gmail.com>
2026-07-25 12:16:18 -0400

    Dynamite Timer

--- Assets/Scripts/Interface/TimerScript.cs
+++ Assets/Scripts/Interface/TimerScript.cs
@@ -1,16 +1,72 @@
+using TMPro;
 using UnityEngine;
 
 public class TimerScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //mostly copied from BombCountdown, id delete but keeping in just in case for the time being.
+    private TMP_Text countdownText;
+    private float timeRemaining;
+    private bool isTimerRunning = false;
+    [SerializeField] private int countdownTime = 30;
+
     void Start()
     {
-        
+        countdownText = GetComponentInChildren<TMP_Text>();
+        Init(countdownTime);
     }
 
-    // Update is called once per frame
+    public void Init(int seconds)
+    {
+        if (GameController.instance.timerEnabled)
+        {
+            timeRemaining = seconds;
+            isTimerRunning = true;
+        }
+    }
+
     void Update()
     {
-        
+        if(isTimerRunning)
+        {
+            if(timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime();
+            }
+            else
+            {
+                timeRemaining = 0;
+                isTimerRunning = false;
+                OnTimerFinish();
+            }
+        } 
+        else
+        {
+            countdownText.color = Color.green;
+        }
+    }
+
+    public void DefuseBomb()
+    {
+        isTimerRunning = false;
+    }
+
+    private void DisplayTime()
+    {
+        float seconds = Mathf.CeilToInt(timeRemaining);
+        if (Mathf.Approximately(seconds, 0))
+        {
+            countdownText.text = "BOOM!";
+        }
+        else
+        {
+            countdownText.text = seconds.ToString();
+        }
+
+    }
+
+    private void OnTimerFinish()
+    {
+        Debug.Log("TODO: Implement game over state");
     }
 }
